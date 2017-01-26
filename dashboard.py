778eda715515becf776cb0ee4afa9e0b17ea054f
Noah Kaplan <noahkaplan4@gmail.com>
2017-01-26 03:54:07 -0500

    add database

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv, find_dotenv
 from twilio import twiml
 
 app = Flask(__name__)
+
+load_dotenv(find_dotenv())
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 @app.route('/')
 def home():
@@ -15,6 +23,16 @@ def sms():
     resp = twiml.Response()
     resp.message('received: %s' % message_body)
     return str(resp)
+
+class Quote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), unique=True)
+
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return '<Quote %r>' % self.text
 
 if __name__ == '__main__':
     app.run()
