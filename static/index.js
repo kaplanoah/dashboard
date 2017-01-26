a42cc4044deff961f5494fedcbb5f7f6f01ab4b3
Noah Kaplan <noahkaplan4@gmail.com>
2017-01-26 15:07:59 -0500

    update displayed latest quote

--- static/index.js
+++ static/index.js
@@ -1,1 +1,12 @@
-console.log('hello');
+(function updateQuote() {
+    $.ajax({
+        url: $SCRIPT_ROOT + '/latest-quote',
+        success: function(data) {
+            console.log(data);
+            $('#jordan').text(data.latest_quote);
+        },
+        complete: function() {
+            setTimeout(updateQuote, 5000);
+        }
+    });
+})();
