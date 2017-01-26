fd41f595affb6324cf3b6a492a614f5031fa8f10
Noah Kaplan <noahkaplan4@gmail.com>
2017-01-25 19:15:56 -0500

    receieve sms

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from twilio import twiml
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/jordan-quote', methods=['POST'])
+def sms():
+    number = request.form['From']
+    message_body = request.form['Body']
+
+    resp = twiml.Response()
+    resp.message('received: %s' % message_body)
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
