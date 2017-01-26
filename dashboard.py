66e38b94e902e35944e98435d2dd4b457b13fedb
Noah Kaplan <noahkaplan4@gmail.com>
2017-01-26 04:38:15 -0500

    create quote

--- dashboard.py
+++ dashboard.py
@@ -11,19 +11,6 @@ load_dotenv(find_dotenv())
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 db = SQLAlchemy(app)
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/jordan-quote', methods=['POST'])
-def sms():
-    number = request.form['From']
-    message_body = request.form['Body']
-
-    resp = twiml.Response()
-    resp.message('received: %s' % message_body)
-    return str(resp)
-
 class Quote(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(500), unique=True)
@@ -33,6 +20,22 @@ class Quote(db.Model):
 
     def __repr__(self):
         return '<Quote %r>' % self.text
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/jordan-quote', methods=['POST'])
+def sms():
+    number = request.form['From']
+    quote_text = request.form['Body']
+
+    db.session.add(Quote(quote_text))
+    db.session.commit()
+
+    resp = twiml.Response()
+    resp.message('received: %s' % message_body)
+    return str(resp)
 
 if __name__ == '__main__':
     app.run()
