7f91565330e3d96f69919702ac11b5ba5500995b
Noah Kaplan <noahkaplan4@gmail.com>
2017-05-08 12:00:26 -0400

    increase timeout

--- static/index.js
+++ static/index.js
@@ -6,7 +6,7 @@
             $('#jordan').text(data.latest_quote);
         },
         complete: function() {
-            setTimeout(updateQuote, 120000);
+            setTimeout(updateQuote, 1800000);
         }
     });
 })();
